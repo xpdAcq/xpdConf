9a921bab620864134ff42dc4ec11d6c3622c95e1
christopher <cjwright4242@gmail.com>
2018-12-03 11:46:46 -0500

    black

--- run_tests.py
+++ run_tests.py
@@ -3,18 +3,18 @@ import sys
 import pytest
 import os
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # show output results from every test function
-    args = ['-v']
+    args = ["-v"]
     # show the message output for skipped and expected failure tests
     if len(sys.argv) > 1:
         args.extend(sys.argv[1:])
-    print('pytest arguments: {}'.format(args))
+    print("pytest arguments: {}".format(args))
     # # compute coverage stats for xpdAcq
     # args.extend(['--cov', 'xpdAcq'])
     # call pytest and exit with the return code from pytest so that
     # travis will fail correctly if tests fail
-    os.environ['XPDAN_SETUP'] = str(1)
+    os.environ["XPDAN_SETUP"] = str(1)
     exit_res = pytest.main(args)
-    os.environ['XPDAN_SETUP'] = str(0)
+    os.environ["XPDAN_SETUP"] = str(0)
     sys.exit(exit_res)
