b28833cddd15958845b99e9eb811d1e9cc34ced6
Timothy Liu <chbrahmsliu@gmail.com>
2019-09-06 14:46:19 -0400

    TST: test pyyaml loading syntax. ensure no warning

---
+++ xpdconf/tests/test_loadyaml.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+import pytest
+from pkg_resources import resource_filename as rs_fn
+from pkg_resources import parse_version
+
+EXP_DIR = rs_fn("xpdconf", "examples/")
+
+def test_yaml_load():
+    # test version
+    assert parse_version(yaml.__version__) > parse_version('3.13')
+    # assert no warning
+    with pytest.warns(None) as record:
+        fn = os.path.join(EXP_DIR, 'sim.yaml')
+        with open(fn) as f:
+            load_d = yaml.full_load(f)
+    assert not record
