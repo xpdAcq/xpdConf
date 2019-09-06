1975fbfc084c8acfc423526667c5ec1f45afe6c7
Timothy Liu <chbrahmsliu@gmail.com>
2019-09-06 15:22:24 -0400

    TST: refactor test. no pyyaml > 3.13 for py3.5

--- xpdconf/tests/test_loadyaml.py
+++ xpdconf/tests/test_loadyaml.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import pytest
+from functools import partial
 from pkg_resources import resource_filename as rs_fn
 from pkg_resources import parse_version
 
@@ -9,10 +10,13 @@ EXP_DIR = rs_fn("xpdconf", "examples/")
 
 def test_yaml_load():
     # test version
-    assert parse_version(yaml.__version__) > parse_version('3.13')
+    if parse_version(yaml.__version__) > parse_version('3.13'):
+        loader = partial(yaml.full_load)
+    else:
+        loader = partial(yaml.load)
     # assert no warning
     with pytest.warns(None) as record:
         fn = os.path.join(EXP_DIR, 'sim.yaml')
         with open(fn) as f:
-            yaml.full_load(f)
+            loader(f)
     assert not record
