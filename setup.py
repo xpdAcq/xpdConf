d069a8c81e013720c30a6caf40fc6294c8848127
st3107 <st3107@columbia.edu>
2021-03-02 16:29:04 -0500

    bumped version to 0.4.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xpdconf",
-    version='0.4.4',
+    version='0.4.5',
     packages=find_packages(),
     description="data processing module",
     zip_safe=False,
