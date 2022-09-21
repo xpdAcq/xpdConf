5ab2eecd20a900eac371da88f49b221f328a07e9
st3107 <st3107@columbia.edu>
2022-09-21 10:11:35 -0400

    MNT: update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 conda create -n "$1" --yes
-conda install -n "$1" --file requirements/build.txt --file requirements/run.txt --file requirements/test.txt --yes
+conda install -n "$1" --file requirements/build.txt --file requirements/run.txt --file requirements/test.txt -c conda-forge --yes
 conda run -n "$1" pip instal -e ./ --no-deps --no-capture-output
