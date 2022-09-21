a368d9654bf8540a40059cafeece38bda7bd24b0
st3107 <st3107@columbia.edu>
2022-09-21 10:11:01 -0400

    BUG: correct install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-conda create -n xpdconf --yes
-conda install -n xpdconf --file requirements/build.txt --file requirements/run.txt --file requirements/test.txt --yes
-conda run -n xpdconf pip instal -e ./ --no-deps --no-capture-output
+#!/bin/bash
+
+conda create -n "$1" --yes
+conda install -n "$1" --file requirements/build.txt --file requirements/run.txt --file requirements/test.txt --yes
+conda run -n "$1" pip instal -e ./ --no-deps --no-capture-output
